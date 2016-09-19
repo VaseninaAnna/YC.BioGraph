656eee0712ecb516d1451543858e8799b63ce975
Fenex (Mallabaev Azamat) <mallabaev.an@yandex.ru>
2016-09-19 14:33:14 +0300

    Init by ProjectScaffold

---
+++ src/YC.BioGraph/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("YC.BioGraph")>]
+[<assembly: AssemblyProductAttribute("YC.BioGraph")>]
+[<assembly: AssemblyDescriptionAttribute("Bio graph parsing web demo")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
+    let [<Literal>] InformationalVersion = "1.0"
