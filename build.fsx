6cee05939505cc13e495a7df0ede310e6b01e91b
Fenex (Mallabaev Azamat) <mallabaev.an@yandex.ru>
2016-09-19 15:14:02 +0300

    Edit owner

--- build.fsx
+++ build.fsx
@@ -52,7 +52,7 @@ let testAssemblies = "tests/**/bin/Release/*Tests*.dll"
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted
-let gitOwner = "YaccConstructor"
+let gitOwner = "Bio-graph-group"
 let gitHome = sprintf "%s/%s" "https://github.com" gitOwner
 
 // The name of the project on GitHub
