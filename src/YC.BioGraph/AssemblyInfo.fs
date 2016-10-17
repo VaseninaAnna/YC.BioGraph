f4c69bb04bf06a4cf344fcbd7de4dbbafbfaa822
Fenex (Mallabaev Azamat) <mallabaev.an@yandex.ru>
2016-10-17 12:54:57 +0300

    add websharper package

--- src/YC.BioGraph/AssemblyInfo.fs
+++ src/YC.BioGraph/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("YC.BioGraph")>]
 [<assembly: AssemblyProductAttribute("YC.BioGraph")>]
 [<assembly: AssemblyDescriptionAttribute("Bio graph parsing web demo")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
-    let [<Literal>] InformationalVersion = "1.0"
+    let [<Literal>] Version = "0.0"
+    let [<Literal>] InformationalVersion = "0.0"
