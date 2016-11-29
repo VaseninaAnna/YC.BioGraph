3e45b321929cbd17fc09bbd7056330584985a0ca
Polina <lunina_polina@mail.ru>
2016-11-29 19:09:13 +0300

    textbox is big ura

--- src/BioGraph/BioGraph/Client.fs
+++ src/BioGraph/BioGraph/Client.fs
@@ -27,79 +27,74 @@ module Client =
                 stateChanged.Trigger(IntelliFactory.Formlets.Base.Result.Success "")
             input, reset, stateChanged.Publish
         |> wsff.InitWith ""//the most important part of the code, don't ever delete it!
+    
+    let InputControl lbl =
+        wsff.Do {
+            let! textInput =
+                wsfc.TextArea ""                    
+                |> wsfe.WithTextLabel lbl
+                |> wsfe.WithLabelAbove
+                |> wsff.MapElement (fun e ->
+                    JQuery.JQuery.Of(e.Dom.QuerySelector("textarea"))
+                        .Css("height", "200px")
+                        .Css("width", "500px")
+                        .Ignore
+                    e)
+            let! fileInput = FileControl
+            return (textInput, fileInput)             
+        }
+        |> wsff.Vertical
+        |> wsfe.WithFormContainer
 
-    let frm =
-        let fileForm =
-            wsff.Do { 
-              let! formlet =
-                FileControl 
-              return formlet
-            }
+    let RangeControl =
+        wsff.Do {                
+            let! min = wsfc.Input "1" |> wsfe.WithTextLabel "from" |>  wsfd.Validator.IsInt "Enter numericr value" 
+            let! max  = wsfc.Input "5" |> wsfe.WithTextLabel "to" |>  wsfd.Validator.IsInt "Enter numericr value" 
+            return (int min, int max)
+        }
+        |> wsff.Horizontal 
+        |> wsfe.WithTextLabel "String range"
+        |> wsfe.WithLabelAbove 
+        |> wsfe.WithFormContainer 
 
-        let RangeForm =
-            wsff.Do {                
-                let! min = wsfc.Input "1" |> wsfe.WithTextLabel "from   " |>  wsfd.Validator.IsInt "Enter numericr value" 
-                let! max  = wsfc.Input "5" |> wsfe.WithTextLabel "to   " |>  wsfd.Validator.IsInt "Enter numericr value" 
-                return (int min, int max)
-            }
-            |> wsff.Horizontal 
-            |> wsfe.WithTextLabel "String range"
-            |> wsfe.WithLabelAbove  
-          
-        let input lbl =
-            wsfc.TextArea ""                    
-            |> wsfe.WithTextLabel lbl
-            |> wsfe.WithLabelAbove
-            |> wsff.MapElement (fun e ->
-                JQuery.JQuery.Of(e.Dom.QuerySelector("textarea"))
-                    .Css("height", "200px")
-                    .Css("width", "500px")
-                    .Ignore
-                e)
-
+    let OutputControl lbl flag = //flag - текст или картинка
+        wsff.Do {
+            let! output =
+                wsfc.ReadOnlyTextArea""                    
+                |> wsfe.WithTextLabel lbl
+                |> wsfe.WithLabelAbove 
+                |> wsff.MapElement (fun e ->
+                    JQuery.JQuery.Of(e.Dom.QuerySelector("textarea"))
+                            .Css("height", "280px")
+                            .Css("width", "500px")
+                            .Ignore
+                    e)
+            return output
+        }
+        |> wsfe.WithFormContainer
+    
+ 
+    let frm =        
+       
         let InputForm  =
-            wsff.Do {
-                 
-                let! grammar = input "Grammar"
-                let! file1 = fileForm
-                let! graph = input "Graph"
-                let! file2 = fileForm 
-                let! range = RangeForm
+            wsff.Do {                
+                let! grammar = InputControl "Grammar"
+                let! graph = InputControl "Graph"
+                let! range = RangeControl
                 let! checkbox = wsfc.Checkbox false |> wsfe.WithTextLabel "DRAW GRAPH" |> wsfe.WithLabelLeft
-                return (grammar,file1, graph, file2, range, checkbox)
+                return (grammar, graph,  range, checkbox)
             }
             |> wsff.Vertical
-            |> wsfe.WithCustomFormContainer({wsfe.FormContainerConfiguration.Default with CssClass=Some"totop"}) 
 
         let OutputForm =
             wsff.Do {
-                let! picture = 
-                    wsfc.ReadOnlyTextArea""                    
-                    |> wsfe.WithTextLabel "Graph visualisation"
-                    |> wsfe.WithLabelAbove 
-                    |> wsff.MapElement (fun e ->
-                         JQuery.JQuery.Of(e.Dom.QuerySelector("textarea"))
-                            .Css("height", "200px")
-                            .Css("width", "500px")
-                            .Ignore
-                         e)
-
-                let! output = 
-                    wsfc.ReadOnlyTextArea ""                    
-                    |> wsfe.WithTextLabel "Output"
-                    |> wsfe.WithLabelAbove 
-                    |> wsfe.WithCustomFormContainer ({ wsfe.FormContainerConfiguration.Default with CssClass=Some"totop"})
-                    |> wsff.MapElement (fun e ->
-                         JQuery.JQuery.Of(e.Dom.QuerySelector("textarea"))
-                            .Css("height", "200px")
-                            .Css("width", "500px")
-                            .Ignore
-                         e)
+                let! picture = OutputControl "Graph visualisation" "image"
+                let! output = OutputControl "Output" "text"
                 return (picture, output)
             }
             |> wsff.Vertical 
-            |> wsfe.WithCustomFormContainer({wsfe.FormContainerConfiguration.Default with CssClass=Some"totop"})
         
+
         wsff.Do {
             let! x = InputForm
             let! y = OutputForm
@@ -109,6 +104,7 @@ module Client =
         |> wsfe.WithCustomSubmitButton ({ wsfe.FormButtonConfiguration.Default with 
                                                                                    Label = Some "GO" 
                                                                                    Style=Some"background-color: #FF1493;font-size: 40px"})
+
 
     let Main () =
         let MainForm =
--- src/BioGraph/BioGraph/Scripts/WebSharper/BioGraph.js
+++ src/BioGraph/BioGraph/Scripts/WebSharper/BioGraph.js
@@ -1,6 +1,6 @@
 (function()
 {
- var Global=this,Runtime=this.IntelliFactory.Runtime,Control,FSharpEvent,List,Html,Client,Attr,Tags,IntelliFactory,Formlets,Base,Result,EventsPervasives,Formlets1,Formlet,BioGraph,Client1,Controls,Enhance,Data,jQuery,FormContainerConfiguration,FormButtonConfiguration;
+ var Global=this,Runtime=this.IntelliFactory.Runtime,Control,FSharpEvent,List,Html,Client,Attr,Tags,IntelliFactory,Formlets,Base,Result,EventsPervasives,Formlets1,Formlet,jQuery,Controls,Enhance,BioGraph,Client1,Data,FormButtonConfiguration;
  Runtime.Define(Global,{
   BioGraph:{
    Client:{
@@ -38,6 +38,32 @@
      formlet=Formlet.BuildFormlet(f);
      return Formlet.InitWith("",formlet);
     }),
+    InputControl:function(lbl)
+    {
+     var _builder_,formlet,formlet4;
+     _builder_=Formlet.Do();
+     formlet=_builder_.Delay(function()
+     {
+      var f,formlet1,formlet2,formlet3;
+      f=function(e)
+      {
+       jQuery(e.Dom.querySelector("textarea")).css("height","200px").css("width","500px");
+       return e;
+      };
+      formlet1=Controls.TextArea("");
+      formlet2=Enhance.WithTextLabel(lbl,formlet1);
+      formlet3=Enhance.WithLabelAbove(formlet2);
+      return _builder_.Bind(Formlet.MapElement(f,formlet3),function(_arg1)
+      {
+       return _builder_.Bind(Client1.FileControl(),function(_arg2)
+       {
+        return _builder_.Return([_arg1,_arg2]);
+       });
+      });
+     });
+     formlet4=Formlet.Vertical(formlet);
+     return Enhance.WithFormContainer(formlet4);
+    },
     Main:function()
     {
      var arg10;
@@ -47,165 +73,102 @@
      })]);
      return Tags.Tags().NewTag("div",arg10);
     },
-    frm:Runtime.Field(function()
+    OutputControl:function(lbl)
     {
-     var _builder_,fileForm,_builder_1,formlet,formlet3,formlet4,RangeForm,input,_builder_2,formlet6,x2,inputRecord,CssClass,fc,InputForm,_builder_3,formlet7,x3,inputRecord2,CssClass2,fc2,OutputForm,_builder_4,formleta,x4,inputRecord3,buttonConf;
+     var _builder_,formlet;
      _builder_=Formlet.Do();
-     fileForm=_builder_.Delay(function()
+     formlet=_builder_.Delay(function()
      {
-      return _builder_.Bind(Client1.FileControl(),function(_arg1)
+      var f,formlet1,formlet2,formlet3;
+      f=function(e)
+      {
+       jQuery(e.Dom.querySelector("textarea")).css("height","280px").css("width","500px");
+       return e;
+      };
+      formlet1=Controls.ReadOnlyTextArea("");
+      formlet2=Enhance.WithTextLabel(lbl,formlet1);
+      formlet3=Enhance.WithLabelAbove(formlet2);
+      return _builder_.Bind(Formlet.MapElement(f,formlet3),function(_arg1)
       {
        return _builder_.Return(_arg1);
       });
      });
-     _builder_1=Formlet.Do();
-     formlet=_builder_1.Delay(function()
+     return Enhance.WithFormContainer(formlet);
+    },
+    RangeControl:Runtime.Field(function()
+    {
+     var _builder_,formlet,formlet3,formlet4,formlet5;
+     _builder_=Formlet.Do();
+     formlet=_builder_.Delay(function()
      {
       var formlet1,x;
       formlet1=Controls.Input("1");
-      x=Enhance.WithTextLabel("from   ",formlet1);
-      return _builder_1.Bind((Data.Validator().IsInt("Enter numericr value"))(x),function(_arg2)
+      x=Enhance.WithTextLabel("from",formlet1);
+      return _builder_.Bind((Data.Validator().IsInt("Enter numericr value"))(x),function(_arg1)
       {
        var formlet2,x1;
        formlet2=Controls.Input("5");
-       x1=Enhance.WithTextLabel("to   ",formlet2);
-       return _builder_1.Bind((Data.Validator().IsInt("Enter numericr value"))(x1),function(_arg3)
+       x1=Enhance.WithTextLabel("to",formlet2);
+       return _builder_.Bind((Data.Validator().IsInt("Enter numericr value"))(x1),function(_arg2)
        {
-        return _builder_1.Return([_arg2<<0,_arg3<<0]);
+        return _builder_.Return([_arg1<<0,_arg2<<0]);
        });
       });
      });
      formlet3=Formlet.Horizontal(formlet);
      formlet4=Enhance.WithTextLabel("String range",formlet3);
-     RangeForm=Enhance.WithLabelAbove(formlet4);
-     input=function(lbl)
+     formlet5=Enhance.WithLabelAbove(formlet4);
+     return Enhance.WithFormContainer(formlet5);
+    }),
+    frm:Runtime.Field(function()
+    {
+     var _builder_,formlet,InputForm,_builder_1,formlet3,OutputForm,_builder_2,formlet4,x,inputRecord,buttonConf;
+     _builder_=Formlet.Do();
+     formlet=_builder_.Delay(function()
      {
-      var f,formlet1,formlet2,formlet5;
-      f=function(e)
+      return _builder_.Bind(Client1.InputControl("Grammar"),function(_arg1)
       {
-       jQuery(e.Dom.querySelector("textarea")).css("height","200px").css("width","500px");
-       return e;
-      };
-      formlet1=Controls.TextArea("");
-      formlet2=Enhance.WithTextLabel(lbl,formlet1);
-      formlet5=Enhance.WithLabelAbove(formlet2);
-      return Formlet.MapElement(f,formlet5);
-     };
-     _builder_2=Formlet.Do();
-     formlet6=_builder_2.Delay(function()
-     {
-      return _builder_2.Bind(input("Grammar"),function(_arg4)
-      {
-       return _builder_2.Bind(fileForm,function(_arg5)
+       return _builder_.Bind(Client1.InputControl("Graph"),function(_arg2)
        {
-        return _builder_2.Bind(input("Graph"),function(_arg6)
+        return _builder_.Bind(Client1.RangeControl(),function(_arg3)
         {
-         return _builder_2.Bind(fileForm,function(_arg7)
+         var formlet1,formlet2;
+         formlet1=Controls.Checkbox(false);
+         formlet2=Enhance.WithTextLabel("DRAW GRAPH",formlet1);
+         return _builder_.Bind(Enhance.WithLabelLeft(formlet2),function(_arg4)
          {
-          return _builder_2.Bind(RangeForm,function(_arg8)
-          {
-           var formlet1,formlet2;
-           formlet1=Controls.Checkbox(false);
-           formlet2=Enhance.WithTextLabel("DRAW GRAPH",formlet1);
-           return _builder_2.Bind(Enhance.WithLabelLeft(formlet2),function(_arg9)
-           {
-            return _builder_2.Return([_arg4,_arg5,_arg6,_arg7,_arg8,_arg9]);
-           });
-          });
+          return _builder_.Return([_arg1,_arg2,_arg3,_arg4]);
          });
         });
        });
       });
      });
-     x2=Formlet.Vertical(formlet6);
-     inputRecord=FormContainerConfiguration.get_Default();
-     CssClass={
-      $:1,
-      $0:"totop"
-     };
-     fc=Runtime.New(FormContainerConfiguration,{
-      Header:inputRecord.Header,
-      Padding:inputRecord.Padding,
-      Description:inputRecord.Description,
-      BackgroundColor:inputRecord.BackgroundColor,
-      BorderColor:inputRecord.BorderColor,
-      CssClass:CssClass,
-      Style:inputRecord.Style
-     });
-     InputForm=Enhance.WithCustomFormContainer(fc,x2);
-     _builder_3=Formlet.Do();
-     formlet7=_builder_3.Delay(function()
+     InputForm=Formlet.Vertical(formlet);
+     _builder_1=Formlet.Do();
+     formlet3=_builder_1.Delay(function()
      {
-      var f,formlet1,formlet2,formlet5;
-      f=function(e)
+      return _builder_1.Bind(Client1.OutputControl("Graph visualisation","image"),function(_arg5)
       {
-       jQuery(e.Dom.querySelector("textarea")).css("height","200px").css("width","500px");
-       return e;
-      };
-      formlet1=Controls.ReadOnlyTextArea("");
-      formlet2=Enhance.WithTextLabel("Graph visualisation",formlet1);
-      formlet5=Enhance.WithLabelAbove(formlet2);
-      return _builder_3.Bind(Formlet.MapElement(f,formlet5),function(_arg10)
-      {
-       var formlet8,formlet9,x,inputRecord1,CssClass1,fc1,x1,f1;
-       formlet8=Controls.ReadOnlyTextArea("");
-       formlet9=Enhance.WithTextLabel("Output",formlet8);
-       x=Enhance.WithLabelAbove(formlet9);
-       inputRecord1=FormContainerConfiguration.get_Default();
-       CssClass1={
-        $:1,
-        $0:"totop"
-       };
-       fc1=Runtime.New(FormContainerConfiguration,{
-        Header:inputRecord1.Header,
-        Padding:inputRecord1.Padding,
-        Description:inputRecord1.Description,
-        BackgroundColor:inputRecord1.BackgroundColor,
-        BorderColor:inputRecord1.BorderColor,
-        CssClass:CssClass1,
-        Style:inputRecord1.Style
-       });
-       x1=Enhance.WithCustomFormContainer(fc1,x);
-       f1=function(e)
+       return _builder_1.Bind(Client1.OutputControl("Output","text"),function(_arg6)
        {
-        jQuery(e.Dom.querySelector("textarea")).css("height","200px").css("width","500px");
-        return e;
-       };
-       return _builder_3.Bind(Formlet.MapElement(f1,x1),function(_arg11)
-       {
-        return _builder_3.Return([_arg10,_arg11]);
+        return _builder_1.Return([_arg5,_arg6]);
        });
       });
      });
-     x3=Formlet.Vertical(formlet7);
-     inputRecord2=FormContainerConfiguration.get_Default();
-     CssClass2={
-      $:1,
-      $0:"totop"
-     };
-     fc2=Runtime.New(FormContainerConfiguration,{
-      Header:inputRecord2.Header,
-      Padding:inputRecord2.Padding,
-      Description:inputRecord2.Description,
-      BackgroundColor:inputRecord2.BackgroundColor,
-      BorderColor:inputRecord2.BorderColor,
-      CssClass:CssClass2,
-      Style:inputRecord2.Style
-     });
-     OutputForm=Enhance.WithCustomFormContainer(fc2,x3);
-     _builder_4=Formlet.Do();
-     formleta=_builder_4.Delay(function()
+     OutputForm=Formlet.Vertical(formlet3);
+     _builder_2=Formlet.Do();
+     formlet4=_builder_2.Delay(function()
      {
-      return _builder_4.Bind(InputForm,function(_arg12)
+      return _builder_2.Bind(InputForm,function(_arg7)
       {
-       return _builder_4.Bind(OutputForm,function(_arg13)
+       return _builder_2.Bind(OutputForm,function(_arg8)
        {
-        return _builder_4.Return([_arg12,_arg13]);
+        return _builder_2.Return([_arg7,_arg8]);
        });
       });
      });
-     x4=Formlet.Horizontal(formleta);
-     inputRecord3=FormButtonConfiguration.get_Default();
+     x=Formlet.Horizontal(formlet4);
+     inputRecord=FormButtonConfiguration.get_Default();
      buttonConf=Runtime.New(FormButtonConfiguration,{
       Label:{
        $:1,
@@ -215,9 +178,9 @@
        $:1,
        $0:"background-color: #FF1493;font-size: 40px"
       },
-      Class:inputRecord3.Class
+      Class:inputRecord.Class
      });
-     return Enhance.WithCustomSubmitButton(buttonConf,x4);
+     return Enhance.WithCustomSubmitButton(buttonConf,x);
     })
    }
   }
@@ -238,18 +201,18 @@
   EventsPervasives=Runtime.Safe(Client.EventsPervasives);
   Formlets1=Runtime.Safe(Global.WebSharper.Formlets);
   Formlet=Runtime.Safe(Formlets1.Formlet);
-  BioGraph=Runtime.Safe(Global.BioGraph);
-  Client1=Runtime.Safe(BioGraph.Client);
+  jQuery=Runtime.Safe(Global.jQuery);
   Controls=Runtime.Safe(Formlets1.Controls);
   Enhance=Runtime.Safe(Formlets1.Enhance);
+  BioGraph=Runtime.Safe(Global.BioGraph);
+  Client1=Runtime.Safe(BioGraph.Client);
   Data=Runtime.Safe(Formlets1.Data);
-  jQuery=Runtime.Safe(Global.jQuery);
-  FormContainerConfiguration=Runtime.Safe(Enhance.FormContainerConfiguration);
   return FormButtonConfiguration=Runtime.Safe(Enhance.FormButtonConfiguration);
  });
  Runtime.OnLoad(function()
  {
   Client1.frm();
+  Client1.RangeControl();
   Client1.FileControl();
   return;
  });
