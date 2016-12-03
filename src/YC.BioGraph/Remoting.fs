583af250880ae4b0d45d9a6317dcdf53b1d07fe9
Азамат Маллабаев < >
2016-12-03 23:39:50 +0300

    Load server stub

--- src/YC.BioGraph/Remoting.fs
+++ src/YC.BioGraph/Remoting.fs
@@ -3,6 +3,26 @@ namespace YC.BioGraph
 open WebSharper
 
 module Server =
+    type Nuc = | A | C | G | U
+
+    type Edge = Edge of s:int * t:int * n:Nuc * f:bool
+
+    type t =
+        | Bad
+        | Normal of int
+        | Good of int
+
+    type QEdge = QEdge of s:int * tA:t * tC:t * TG:t * TU:t
+
+    type Graph = {
+        countOfVertex: int;
+        edges: Edge[];
+        qedges: QEdge[];
+    }
+
+    type Result =
+        | Error of message: string
+        | Success of optGraph: option<Graph> * seqs: string[]
 
     [<Rpc>]
     let DoSomething input =
@@ -10,3 +30,18 @@ module Server =
         async {
             return R input
         }
+
+    [<Rpc>]
+    let LoadDefaultFiles () =
+        [| "A"; "B" |]
+
+    [<Rpc>]
+    let LoadDefaultFile name =
+        match name with
+        | "A" -> "AAA"
+        | "B" -> "BBB"
+        |  _  -> ""
+
+    [<Rpc>]
+    let Parse (grammar: string) (graph: string) (range: int * int) (isOutputGraph: bool) =
+        Error "!!?"
