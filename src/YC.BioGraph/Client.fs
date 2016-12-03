a79e2ab25e7e1e65261ac8d43d5fe5e6d0068c86
Polina <lunina_polina@mail.ru>
2016-12-03 21:26:31 +0300

    fix

--- src/YC.BioGraph/Client.fs
+++ src/YC.BioGraph/Client.fs
@@ -1,26 +1,114 @@
 namespace YC.BioGraph
 
+
 open WebSharper
 open WebSharper.JavaScript
-open WebSharper.UI.Next
-open WebSharper.UI.Next.Client
-open WebSharper.UI.Next.Html
+open WebSharper.Html.Client
+
+module wsfc = WebSharper.Formlets.Controls
+module wsfe = WebSharper.Formlets.Enhance
+module wsfd = WebSharper.Formlets.Data
+module wsff = WebSharper.Formlets.Formlet
+module wsfl = WebSharper.Formlets.Layout
+
+
 
 [<JavaScript>]
 module Client =
 
+    let ButtonControl =
+       wsff.OfElement (fun () ->
+            Button[Text "Choose default"]
+        )
+   
+    let FileControl = 
+        wsff.OfElement (fun () ->
+            Input [Attr.Type "file"]
+        )
+    
+    let InputControl lbl =
+        wsff.Do {
+            let! textInput =
+                wsfc.TextArea ""                    
+                |> wsfe.WithTextLabel lbl
+                |> wsfe.WithLabelAbove
+                |> wsff.MapElement (fun e ->
+                    JQuery.JQuery.Of(e.Dom.QuerySelector("textarea"))
+                        .Css("height", "200px")
+                        .Css("width", "500px")
+                        .Ignore
+                    e)
+            let! fileInput = FileControl
+            let! chooseButton = ButtonControl
+            return (textInput, fileInput, chooseButton)             
+        }
+        |> wsff.Vertical
+        |> wsfe.WithFormContainer
+
+    let RangeControl =
+        wsff.Do {                
+            let! min = wsfc.Input "1" |> wsfe.WithTextLabel "from" |> wsfd.Validator.IsInt "Enter numericr value" 
+            let! max  = wsfc.Input "5" |> wsfe.WithTextLabel "to" |> wsfd.Validator.IsInt "Enter numericr value" 
+            return (int min, int max)
+        }
+        |> wsff.Horizontal 
+        |> wsfe.WithTextLabel "String range"
+        |> wsfe.WithLabelAbove 
+        |> wsfe.WithFormContainer 
+
+    let OutputControl lbl flag = //flag - text/image
+        wsff.Do {
+            let! output =
+                wsfc.ReadOnlyTextArea""                    
+                |> wsfe.WithTextLabel lbl
+                |> wsfe.WithLabelAbove 
+                |> wsff.MapElement (fun e ->
+                    JQuery.JQuery.Of(e.Dom.QuerySelector("textarea"))
+                            .Css("height", "300px")
+                            .Css("width", "500px")
+                            .Ignore
+                    e)
+            return output
+        }
+        |> wsfe.WithFormContainer
+    
+    let frm =        
+     
+        let InputForm  =
+            wsff.Do {                
+                let! grammar = InputControl "Grammar"
+                let! graph = InputControl "Graph"
+                let! range = RangeControl
+                let! checkbox = wsfc.Checkbox false |> wsfe.WithTextLabel "DRAW GRAPH" |> wsfe.WithLabelLeft
+                return (grammar, graph, range, checkbox)
+            }
+            |> wsff.Vertical
+
+        let OutputForm =
+            wsff.Do {
+                let! picture = OutputControl "Graph visualisation" "image"
+                let! output = OutputControl "Output" "text"
+                return (picture, output)
+            }
+            |> wsff.Vertical 
+        
+        wsff.Do {
+            let! x = InputForm
+            let! y = OutputForm
+            return (x, y)
+        }
+        |> wsff.Horizontal
+        |> wsfe.WithCustomSubmitButton ({ wsfe.FormButtonConfiguration.Default with 
+                                                                                   Label = Some "GO" 
+                                                                                   Style=Some "background-color: #FF1493; font-size: 40px"})
+
     let Main () =
-        let rvInput = Var.Create ""
-        let submit = Submitter.CreateOption rvInput.View
-        let vReversed =
-            submit.View.MapAsync(function
-                | None -> async { return "" }
-                | Some input -> Server.DoSomething input
-            )
-        div [
-            Doc.Input [] rvInput
-            Doc.Button "Send" [] submit.Trigger
-            hr []
-            h4Attr [attr.``class`` "text-muted"] [text "The server responded:"]
-            divAttr [attr.``class`` "jumbotron"] [h1 [textView vReversed]]
-        ]
+        let MainForm =
+            frm.Run(fun _ -> ())
+        
+        Div [      
+           MainForm
+        ] 
+
+
+ 
--- src/YC.BioGraph/Scripts/WebSharper/YC.BioGraph.js
+++ src/YC.BioGraph/Scripts/WebSharper/YC.BioGraph.js
@@ -1,60 +1,203 @@
 (function()
 {
- var Global=this,Runtime=this.IntelliFactory.Runtime,UI,Next,Var,Submitter1,Remoting,AjaxRemotingProvider,Concurrency,View,List,Doc,T,AttrProxy;
+ var Global=this,Runtime=this.IntelliFactory.Runtime,Formlets,Formlet,List,Html,Client,Tags,Attr,jQuery,Controls,Enhance,YC,BioGraph,Client1,Data,FormButtonConfiguration;
  Runtime.Define(Global,{
   YC:{
    BioGraph:{
     Client:{
+     ButtonControl:Runtime.Field(function()
+     {
+      return Formlet.OfElement(function()
+      {
+       var arg10;
+       arg10=List.ofArray([Tags.Tags().text("Choose default")]);
+       return Tags.Tags().NewTag("button",arg10);
+      });
+     }),
+     FileControl:Runtime.Field(function()
+     {
+      return Formlet.OfElement(function()
+      {
+       var arg10;
+       arg10=List.ofArray([Attr.Attr().NewAttr("type","file")]);
+       return Tags.Tags().NewTag("input",arg10);
+      });
+     }),
+     InputControl:function(lbl)
+     {
+      var _builder_,formlet,formlet4;
+      _builder_=Formlet.Do();
+      formlet=_builder_.Delay(function()
+      {
+       var f,formlet1,formlet2,formlet3;
+       f=function(e)
+       {
+        jQuery(e.Dom.querySelector("textarea")).css("height","200px").css("width","500px");
+        return e;
+       };
+       formlet1=Controls.TextArea("");
+       formlet2=Enhance.WithTextLabel(lbl,formlet1);
+       formlet3=Enhance.WithLabelAbove(formlet2);
+       return _builder_.Bind(Formlet.MapElement(f,formlet3),function(_arg1)
+       {
+        return _builder_.Bind(Client1.FileControl(),function(_arg2)
+        {
+         return _builder_.Bind(Client1.ButtonControl(),function(_arg3)
+         {
+          return _builder_.Return([_arg1,_arg2,_arg3]);
+         });
+        });
+       });
+      });
+      formlet4=Formlet.Vertical(formlet);
+      return Enhance.WithFormContainer(formlet4);
+     },
      Main:function()
      {
-      var rvInput,submit,arg00,arg10,vReversed,arg20,arg201,arg202,arg203;
-      rvInput=Var.Create("");
-      submit=Submitter1.CreateOption(rvInput.get_View());
-      arg00=function(_arg1)
+      var arg10;
+      arg10=List.ofArray([Client1.frm().Run(function()
       {
-       return _arg1.$==1?AjaxRemotingProvider.Async("YC.BioGraph:0",[_arg1.$0]):Concurrency.Delay(function()
+       return null;
+      })]);
+      return Tags.Tags().NewTag("div",arg10);
+     },
+     OutputControl:function(lbl)
+     {
+      var _builder_,formlet;
+      _builder_=Formlet.Do();
+      formlet=_builder_.Delay(function()
+      {
+       var f,formlet1,formlet2,formlet3;
+       f=function(e)
        {
-        return Concurrency.Return("");
+        jQuery(e.Dom.querySelector("textarea")).css("height","300px").css("width","500px");
+        return e;
+       };
+       formlet1=Controls.ReadOnlyTextArea("");
+       formlet2=Enhance.WithTextLabel(lbl,formlet1);
+       formlet3=Enhance.WithLabelAbove(formlet2);
+       return _builder_.Bind(Formlet.MapElement(f,formlet3),function(_arg1)
+       {
+        return _builder_.Return(_arg1);
        });
-      };
-      arg10=submit.get_View();
-      vReversed=View.MapAsync(arg00,arg10);
-      arg201=function()
-      {
-       return submit.Trigger();
-      };
-      arg202=Runtime.New(T,{
-       $:0
       });
-      arg203=List.ofArray([Doc.TextView(vReversed)]);
-      arg20=List.ofArray([Doc.Input(Runtime.New(T,{
-       $:0
-      }),rvInput),Doc.Button("Send",Runtime.New(T,{
-       $:0
-      }),arg201),Doc.Element("hr",[],arg202),Doc.Element("h4",List.ofArray([AttrProxy.Create("class","text-muted")]),List.ofArray([Doc.TextNode("The server responded:")])),Doc.Element("div",List.ofArray([AttrProxy.Create("class","jumbotron")]),List.ofArray([Doc.Element("h1",[],arg203)]))]);
-      return Doc.Element("div",[],arg20);
-     }
+      return Enhance.WithFormContainer(formlet);
+     },
+     RangeControl:Runtime.Field(function()
+     {
+      var _builder_,formlet,formlet3,formlet4,formlet5;
+      _builder_=Formlet.Do();
+      formlet=_builder_.Delay(function()
+      {
+       var formlet1,x;
+       formlet1=Controls.Input("1");
+       x=Enhance.WithTextLabel("from",formlet1);
+       return _builder_.Bind((Data.Validator().IsInt("Enter numericr value"))(x),function(_arg1)
+       {
+        var formlet2,x1;
+        formlet2=Controls.Input("5");
+        x1=Enhance.WithTextLabel("to",formlet2);
+        return _builder_.Bind((Data.Validator().IsInt("Enter numericr value"))(x1),function(_arg2)
+        {
+         return _builder_.Return([_arg1<<0,_arg2<<0]);
+        });
+       });
+      });
+      formlet3=Formlet.Horizontal(formlet);
+      formlet4=Enhance.WithTextLabel("String range",formlet3);
+      formlet5=Enhance.WithLabelAbove(formlet4);
+      return Enhance.WithFormContainer(formlet5);
+     }),
+     frm:Runtime.Field(function()
+     {
+      var _builder_,formlet,InputForm,_builder_1,formlet3,OutputForm,_builder_2,formlet4,x,inputRecord,buttonConf;
+      _builder_=Formlet.Do();
+      formlet=_builder_.Delay(function()
+      {
+       return _builder_.Bind(Client1.InputControl("Grammar"),function(_arg1)
+       {
+        return _builder_.Bind(Client1.InputControl("Graph"),function(_arg2)
+        {
+         return _builder_.Bind(Client1.RangeControl(),function(_arg3)
+         {
+          var formlet1,formlet2;
+          formlet1=Controls.Checkbox(false);
+          formlet2=Enhance.WithTextLabel("DRAW GRAPH",formlet1);
+          return _builder_.Bind(Enhance.WithLabelLeft(formlet2),function(_arg4)
+          {
+           return _builder_.Return([_arg1,_arg2,_arg3,_arg4]);
+          });
+         });
+        });
+       });
+      });
+      InputForm=Formlet.Vertical(formlet);
+      _builder_1=Formlet.Do();
+      formlet3=_builder_1.Delay(function()
+      {
+       return _builder_1.Bind(Client1.OutputControl("Graph visualisation","image"),function(_arg5)
+       {
+        return _builder_1.Bind(Client1.OutputControl("Output","text"),function(_arg6)
+        {
+         return _builder_1.Return([_arg5,_arg6]);
+        });
+       });
+      });
+      OutputForm=Formlet.Vertical(formlet3);
+      _builder_2=Formlet.Do();
+      formlet4=_builder_2.Delay(function()
+      {
+       return _builder_2.Bind(InputForm,function(_arg7)
+       {
+        return _builder_2.Bind(OutputForm,function(_arg8)
+        {
+         return _builder_2.Return([_arg7,_arg8]);
+        });
+       });
+      });
+      x=Formlet.Horizontal(formlet4);
+      inputRecord=FormButtonConfiguration.get_Default();
+      buttonConf=Runtime.New(FormButtonConfiguration,{
+       Label:{
+        $:1,
+        $0:"GO"
+       },
+       Style:{
+        $:1,
+        $0:"background-color: #FF1493; font-size: 40px"
+       },
+       Class:inputRecord.Class
+      });
+      return Enhance.WithCustomSubmitButton(buttonConf,x);
+     })
     }
    }
   }
  });
  Runtime.OnInit(function()
  {
-  UI=Runtime.Safe(Global.WebSharper.UI);
-  Next=Runtime.Safe(UI.Next);
-  Var=Runtime.Safe(Next.Var);
-  Submitter1=Runtime.Safe(Next.Submitter1);
-  Remoting=Runtime.Safe(Global.WebSharper.Remoting);
-  AjaxRemotingProvider=Runtime.Safe(Remoting.AjaxRemotingProvider);
-  Concurrency=Runtime.Safe(Global.WebSharper.Concurrency);
-  View=Runtime.Safe(Next.View);
+  Formlets=Runtime.Safe(Global.WebSharper.Formlets);
+  Formlet=Runtime.Safe(Formlets.Formlet);
   List=Runtime.Safe(Global.WebSharper.List);
-  Doc=Runtime.Safe(Next.Doc);
-  T=Runtime.Safe(List.T);
-  return AttrProxy=Runtime.Safe(Next.AttrProxy);
+  Html=Runtime.Safe(Global.WebSharper.Html);
+  Client=Runtime.Safe(Html.Client);
+  Tags=Runtime.Safe(Client.Tags);
+  Attr=Runtime.Safe(Client.Attr);
+  jQuery=Runtime.Safe(Global.jQuery);
+  Controls=Runtime.Safe(Formlets.Controls);
+  Enhance=Runtime.Safe(Formlets.Enhance);
+  YC=Runtime.Safe(Global.YC);
+  BioGraph=Runtime.Safe(YC.BioGraph);
+  Client1=Runtime.Safe(BioGraph.Client);
+  Data=Runtime.Safe(Formlets.Data);
+  return FormButtonConfiguration=Runtime.Safe(Enhance.FormButtonConfiguration);
  });
  Runtime.OnLoad(function()
  {
+  Client1.frm();
+  Client1.RangeControl();
+  Client1.FileControl();
+  Client1.ButtonControl();
   return;
  });
 }());
